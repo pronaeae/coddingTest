3ea0c5c03e703a88c462d3581b1d65c028941a16
kdj222 <91784305+kdj222@users.noreply.github.com>
2023-08-10 15:44:38 +0900

    [level 1] Title: 직사각형 별찍기, Time: 49.71 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,10 +2,10 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    // row의 길이를 먼저 구함
-    const col = new Array(a).fill('*').join("")
-    const row = new Array(b).fill(col)
+      const row = '*'.repeat(a)
+    for(let i =0; i < b; i++){
+        console.log(row)
+    }
     
     
-    console.log(row.join('\n'))
 });
