6583830fa6e532f728460ef655d01ded6a7a89b8
kdj222 <91784305+kdj222@users.noreply.github.com>
2023-08-11 16:16:27 +0900

    [unrated] Title: 카드 뭉치, Time: 3.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,28 @@
+function solution(cards1, cards2, goal) {
+    let goalIndex = 0
+    let answer = 'Yes'
+    
+    for(let i = 0; i < goal.length; i++){
+        //만약에 cards1[0]이 goal[index]와 같으면
+        if(cards1[0] === goal[i]){
+            cards1.shift()
+        }
+        //만약에 cards2[0]이 goal[index]와 같으면
+        else if(cards2[0] === goal[i]){
+            cards2.shift()
+        }
+        else{
+            answer = 'No'
+            console.log('같지않음')
+            break;
+        }
+    }
+    
+    console.log(cards1, cards2)
+    
+    //결과 값은 'No'
+    
+    
+   
+    return answer
+}
