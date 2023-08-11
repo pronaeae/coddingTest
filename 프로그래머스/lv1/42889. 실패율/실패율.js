ba3a6983b46bdc4078b9810f92a1491449b0026b
kdj222 <91784305+kdj222@users.noreply.github.com>
2023-08-11 23:41:47 +0900

    [level 1] Title: 실패율, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,8 +1,7 @@
 function solution(N, stages) {
-    var answer = [];
     const stageList = Array.from({length : N},(_,i)=>({stage : i + 1, clear : 0, failed : 0}))
+    
     for(let i = 0; i < stages.length; i++){
-        // console.log(stages[i])
         for(let j = 0; j < stageList.length; j++){
             if(stageList[j].stage === stages[i]){
                 stageList[j]['failed'] += 1
@@ -16,7 +15,6 @@ function solution(N, stages) {
     const newarray = stageList.map(a => ({...a,failed : a.failed/(a.clear+a.failed)}))
                     .sort((a, b) => b.failed - a.failed)
                     .map(a => a.stage)
-    console.log(newarray)
     
     return newarray;
 }
