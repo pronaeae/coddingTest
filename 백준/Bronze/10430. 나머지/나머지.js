fc6788254ad25602fd5477c9f9341538473fdbbc
kdj222 <91784305+kdj222@users.noreply.github.com>
2023-11-20 13:45:05 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var inputData = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+const C = parseInt(inputData[2]);
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
