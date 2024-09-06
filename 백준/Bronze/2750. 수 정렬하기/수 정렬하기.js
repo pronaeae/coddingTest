d113c5cdab4dbba59fe3bfbef5974a41448331be
pronaeae <91784305+pronaeae@users.noreply.github.com>
2024-09-06 13:04:02 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 11252 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -1,12 +1,38 @@
 const [_, ...rest] = require("fs").readFileSync(0, "utf-8").toString().trim().split("\n").map(Number);
 
 function solution(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+  function buildHeap(arr) {
+    const len = arr.length;
+    for (let i = Math.floor(len / 2) - 1; i >= 0; i--) {
+      heapify(arr, len, i);
     }
   }
 
-  for (const item of arr) console.log(item);
+  function heapify(arr, len, idx) {
+    let largest = idx;
+    const left = 2 * idx + 1;
+    const right = 2 * idx + 2;
+    if (left < len && arr[left] > arr[largest]) {
+      largest = left;
+    }
+    if (right < len && arr[right] > arr[largest]) {
+      largest = right;
+    }
+    if (largest !== idx) {
+      [arr[idx], arr[largest]] = [arr[largest], arr[idx]];
+      heapify(arr, len, largest);
+    }
+  }
+  function sort(arr) {
+    const len = arr.length;
+    buildHeap(arr);
+    for (let i = len - 1; i >= 0; i--) {
+      [arr[0], arr[i]] = [arr[i], arr[0]];
+      heapify(arr, i, 0);
+    }
+    return arr;
+  }
+  return sort(arr);
 }
-solution(rest);
+
+console.log(solution(rest).join("\n"));
