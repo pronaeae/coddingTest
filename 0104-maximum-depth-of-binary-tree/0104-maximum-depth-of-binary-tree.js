ef77fbd102aa6e4660921f425762390ce803f23a
pronaeae <91784305+pronaeae@users.noreply.github.com>
2025-03-02 18:30:45 +0900

    Time: 0 ms (100%), Space: 58.1 MB (9.42%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,8 @@
+function maxDepth(root) {
+    if(!root) return 0;
+
+    const leftDepth = maxDepth(root.left);
+    const rightDepth = maxDepth(root.right);
+
+    return Math.max(leftDepth, rightDepth) + 1
+}
