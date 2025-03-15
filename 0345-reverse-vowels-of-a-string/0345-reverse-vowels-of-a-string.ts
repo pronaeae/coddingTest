aa318dd055c9ca94c6d5c87e0e725b1acdfb7d75
pronaeae <91784305+pronaeae@users.noreply.github.com>
2025-03-15 19:31:04 +0900

    Time: 12 ms (29.18%), Space: 75.1 MB (5.01%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
@@ -1,16 +1,16 @@
 function reverseVowels(s: string): string {
-    const filterList = ['a','e','i','o','u'];
+    const filterList = ['a','e','i','o','u','A','E','I','O','U'];
     const splited = s.split('');
 
     function reverse(left: number, right: number){
         if(left >= right) return;
 
-        if(!filterList.includes(splited[left].toLowerCase())){
+        if(!filterList.includes(splited[left])){
             reverse(left + 1, right);
             return;
         };
 
-        if(!filterList.includes(splited[right].toLowerCase())){
+        if(!filterList.includes(splited[right])){
             reverse(left, right - 1);
             return;
         };
