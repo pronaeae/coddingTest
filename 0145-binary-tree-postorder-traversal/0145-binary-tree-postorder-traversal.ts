18b849562ee9f01f956fbc4f46b5981b68e169fb
pronaeae <91784305+pronaeae@users.noreply.github.com>
2025-03-20 18:01:38 +0900

    Time: 0 ms (100%), Space: 55.9 MB (23.28%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+    const result = [];
+
+    function postorder(node){
+        if(node === null) return;
+
+        postorder(node.left);
+        postorder(node.right);
+        result.push(node.val);
+    };
+
+    postorder(root);
+    return result
+};
